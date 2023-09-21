b632190c6e4451d334789ec75a9d2fd4e3434d75
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2023-09-21 23:06:46 +0900

    POC: Add Client (WIP)

---
+++ Tarteeb.Importer/Models/Exceptions/ClientNullException.cs
@@ -0,0 +1,16 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Powering True Leadership
+//===========================
+
+using Xeptions;
+
+namespace Tarteeb.Importer.Models.Exceptions
+{
+    internal class ClientNullException : Xeption
+    {
+        public ClientNullException()
+            : base(message: "Client is null")
+        { }
+    }
+}
---
+++ Tarteeb.Importer/Services/ClientService.cs
@@ -0,0 +1,29 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Powering True Leadership
+//===========================
+
+using System.Threading.Tasks;
+using Tarteeb.Importer.Brokers.Storages;
+using Tarteeb.Importer.Models.Clients;
+using Tarteeb.Importer.Models.Exceptions;
+
+namespace Tarteeb.Importer.Services
+{
+    internal class ClientService
+    {
+        private readonly StorageBroker storageBroker;
+
+        public ClientService() =>
+            this.storageBroker = new StorageBroker();
+
+        internal Task<Client> AddClientAsync(Client client)
+        {
+            if(client is null)
+            {
+                throw new ClientNullException();
+            }
+            return this.storageBroker.InsertClientAsync(client);
+        }
+    }
+}
