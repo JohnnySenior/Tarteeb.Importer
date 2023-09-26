640d20be293c15524652b1ec15b788a8c7887553
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2023-09-26 15:18:28 +0900

    POC: Add Client (WIP)

--- Tarteeb.Importer/Services/ClientService.cs
+++ Tarteeb.Importer/Services/ClientService.cs
@@ -3,6 +3,10 @@
 // Powering True Leadership
 //===========================
 
+using System;
+using System.Data;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Tarteeb.Importer.Brokers.Storages;
 using Tarteeb.Importer.Models.Clients;
@@ -17,13 +21,62 @@ namespace Tarteeb.Importer.Services
         public ClientService() =>
             this.storageBroker = new StorageBroker();
 
+        /// <exception cref="ClientNullException"></exception>
+        /// <exception cref="InvalidCastException"></exception>
+
         internal Task<Client> AddClientAsync(Client client)
         {
             if(client is null)
             {
                 throw new ClientNullException();
             }
+
+            Validate(
+                (Rule: IsInvalid(client.Id), Parameter: nameof(Client.Id)),
+                (Rule: IsInvalid(client.Firstname), Parameter: nameof(Client.Firstname)),
+                (Rule: IsInvalid(client.Lastname), Parameter: nameof(Client.Lastname)),
+                (Rule: IsInvalid(client.Email), Parameter: nameof(Client.Email)),
+                (Rule: IsInvalid(client.GroupId), Parameter: nameof(Client.GroupId)));
+
+            Validate(
+                (Rule: IsInvalidEmail(client.Email), Parameter: nameof(Client.Email)));
+
             return this.storageBroker.InsertClientAsync(client);
+        }
+
+        private dynamic IsInvalid(string text) => new
+        {
+            Condition = string.IsNullOrWhiteSpace(text),
+            Message = "Text is required"
+        };
+
+        private dynamic IsInvalid(Guid id) => new
+        {
+            Condition = id == default,
+            Message = "Id is required"
+        };
+
+        private dynamic IsInvalidEmail(string email) => new
+        {
+            Condition = !Regex.IsMatch(email, "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$\r\n"),
+            Message = "Email is invalid"
+        };
+
+        private void Validate(params(dynamic Rule, string Parameter)[] validations)
+        {
+            var invalidClientException = new InvalidClientException();
+
+            foreach ((dynamic rule, string parameter) in validations)
+            {
+                if(rule.Condition)
+                {
+                    invalidClientException.UpsertDataList(
+                        key: parameter,
+                        value: rule.Message);
+                }
+            }
+
+            invalidClientException.ThrowIfContainsErrors();
         }
     }
 }
