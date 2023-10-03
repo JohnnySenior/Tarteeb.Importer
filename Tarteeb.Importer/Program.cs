a57a725794afacb14d24350047a72697f10f2c71
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2023-10-03 18:55:27 +0900

    EXPOSERS: Add Client

--- Tarteeb.Importer/Program.cs
+++ Tarteeb.Importer/Program.cs
@@ -4,30 +4,63 @@
 //===========================
 
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using EFxceptions.Models.Exceptions;
+using Tarteeb.Importer.Brokers.DateTimes;
+using Tarteeb.Importer.Brokers.Loggings;
 using Tarteeb.Importer.Brokers.Storages;
 using Tarteeb.Importer.Models.Clients;
+using Tarteeb.Importer.Models.Exceptions;
+using Tarteeb.Importer.Services;
 
 namespace Tarteeb.Importer
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            var client = new Client
+            Client client = new Client
             {
-                Id = Guid.NewGuid(),
+                //Id = Guid.NewGuid(),
                 Firstname = "Johnny",
                 Lastname = "Senior",
                 PhoneNumber = "01076419505",
                 Email = "jamshidbektursunboev@gmail.com",
-                BirthDate = DateTimeOffset.Now,
+                BirthDate = DateTimeOffset.Parse("21/05/1995"),
+                GroupId = Guid.NewGuid(),
             };
 
-            using (var storageBroker = new StorageBroker())
+            var loggingBroker = new LoggingBroker();
+            var clientService = new ClientService(new StorageBroker(), new DateTimeBroker());
+
+            try
             {
-                storageBroker.Clients.Add(client);
-                storageBroker.SaveChanges();
+                var persistedClient = await clientService.AddClientAsync(client);
+            }
+            catch (ClientValidationException clientValidationException)
+            {
+                loggingBroker.LogError(clientValidationException.InnerException.Message);
+
+                foreach (DictionaryEntry entry in clientValidationException.InnerException.Data)
+                {
+                    string errorSummary = string.Join(",", (List<string>)entry.Value);
+
+                    loggingBroker.LogError(entry.Key + " => " + errorSummary);
+                }
+            }
+            catch(ClientDependencyValidationException clientDependencyValidationException)
+            {
+                loggingBroker.LogError(clientDependencyValidationException.InnerException.Message);
+            }
+            catch(ClientDependencyException clientDependencyException)
+            {
+                loggingBroker.LogError(clientDependencyException.InnerException.Message);
+            }
+            catch(ClientServiceException clientServiceException)
+            {
+                loggingBroker.LogError(clientServiceException.InnerException.Message);
             }
         }
     }
